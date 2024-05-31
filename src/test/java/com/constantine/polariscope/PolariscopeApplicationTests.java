f790d2326f96c46833fa1273d5b27ce719f426bc
Constantine Khamis <costikhamis@gmail.com>
2024-05-31 16:15:41 -0700

    Initial file structure created

---
+++ src/test/java/com/constantine/polariscope/PolariscopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.constantine.polariscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolariscopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
