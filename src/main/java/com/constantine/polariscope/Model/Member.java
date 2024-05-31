3ea39f1a7d6ede5f05e6936aca5314608babc8eb
Constantine Khamis <costikhamis@gmail.com>
2024-05-31 16:21:26 -0700

    Program runs. Begun implementation of Member object

---
+++ src/main/java/com/constantine/polariscope/Model/Member.java
@@ -0,0 +1,20 @@
+package com.constantine.polariscope.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue
+    private UUID id;
+}
