9529fab5aa14953291b2deb731fd5302815c16cd
Constantine Khamis <costikhamis@gmail.com>
2024-06-02 17:33:12 -0700

    Begun working on login page

--- src/main/java/com/constantine/polariscope/Controller/FrontEndController.java
+++ src/main/java/com/constantine/polariscope/Controller/FrontEndController.java
@@ -1,7 +1,12 @@
 package com.constantine.polariscope.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontEndController {
+    @GetMapping("/login")
+    public String getLogin() {
+        return "pages/main/login";
+    }
 }
