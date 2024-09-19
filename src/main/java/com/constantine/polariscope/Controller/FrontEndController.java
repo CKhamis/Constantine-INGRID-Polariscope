1c9c70fb943717752332a6f805eb9cea908a2266
Constantine Khamis <costikhamis@gmail.com>
2024-09-19 12:51:27 -0700

    New event feature added to Polariscope!

--- src/main/java/com/constantine/polariscope/Controller/FrontEndController.java
+++ src/main/java/com/constantine/polariscope/Controller/FrontEndController.java
@@ -17,7 +17,7 @@ import java.security.Principal;
 @AllArgsConstructor
 public class FrontEndController {
     private final UserService userService;
-    private static final String VERSION = "1.3.3";
+    private static final String VERSION = "1.8.0";
 
     private User getCurrentUser(Principal principal){
         if(principal == null){
