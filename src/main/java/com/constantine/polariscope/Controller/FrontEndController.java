620c9d8c957049dc11540a22df5c67cf02b1c596
Constantine Khamis <costikhamis@gmail.com>
2024-09-20 14:04:29 -0700

    Added a new tab dedicated for events in the Member records screen

--- src/main/java/com/constantine/polariscope/Controller/FrontEndController.java
+++ src/main/java/com/constantine/polariscope/Controller/FrontEndController.java
@@ -17,7 +17,7 @@ import java.security.Principal;
 @AllArgsConstructor
 public class FrontEndController {
     private final UserService userService;
-    private static final String VERSION = "1.8.1";
+    private static final String VERSION = "1.8.5";
 
     private User getCurrentUser(Principal principal){
         if(principal == null){
